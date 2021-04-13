9a0ac8fbdbcdff8a64f3c31344295cf3f4877228
Sean Sundberg <seansund@us.ibm.com>
2021-04-12 22:31:27 -0500

    Updates output values (#2)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "gateway_count" {
+output "count" {
   description = "The number of gateways created"
   value       = local.gateway_count
 }
 
-output "gateway_ids" {
+output "ids" {
   description = "List of ids of gateways created"
   value       = ibm_is_public_gateway.vpc_gateway[*].id
 }
